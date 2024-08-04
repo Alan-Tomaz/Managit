195d0784918a309bf6b38cc40a23290152063c2b
Alan <alan4tomaz8@gmail.com>
2024-08-04 20:40:24 -0300

    Stock Section Finished

--- client/src/components/sections/Dashboard.jsx
+++ client/src/components/sections/Dashboard.jsx
@@ -4,7 +4,7 @@ import { RiMoneyDollarCircleLine } from "react-icons/ri";
 import { GrMoney } from "react-icons/gr";
 import { LuBoxes } from "react-icons/lu";
 import { DiDropbox } from "react-icons/di";
-import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack, IoIosArrowForward, IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import MdTopProducts from '../cards/MdTopProducts';
 /* CHART */
 import Chart from "chart.js/auto";
@@ -17,19 +17,19 @@ Chart.register(CategoryScale);
 function Dashboard() {
 
     const setLabels = [["0", "5", "10", "15", "20", "25", "30"], ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90"], ["Jan", "Feb", "Mar", "Apr", "May", "Jun"], ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Set", "Oct", "Nov", "Dec"]]
-    const setData = [[[7000, 2000, 10000, 13000, 9000, 4000, 8000], [7000, 10000, 9000, 5000, 3000, 13000, 15000, 7000, 19000], [9000, 13000, 19000, 7000, 13000, 17000], [9000, 13000, 19000, 7000, 13000, 17000, 4500, 14354, 17000, 18000, 12000, 16000, 10000]], [[3000, 700, 5000, 10000, 16000, 10000, 13000], [14000, 5000, 16000, 10000, 7000, 6000, 9000, 15000, 9000, 13000], [15000, 10000, 13000, 19000, 15600, 11000], [9000, 16000, 13000, 14000, 12000, 8000, 9000, 12000, 13000, 19000, 17000, 15000]]]
+    const setData = [[[7000, 2000, 10000, 13000, 9000, 4000, 8000], [7000, 10000, 9000, 5000, 3000, 13000, 15000, 7000, 19000, 18000], [9000, 13000, 19000, 7000, 13000, 17000], [9000, 13000, 19000, 7000, 13000, 17000, 4500, 14354, 17000, 18000, 12000, 16000, 10000]], [[3000, 700, 5000, 10000, 16000, 10000, 13000], [14000, 5000, 16000, 10000, 7000, 6000, 9000, 15000, 9000, 13000], [15000, 10000, 13000, 19000, 15600, 11000], [9000, 16000, 13000, 14000, 12000, 8000, 9000, 12000, 13000, 19000, 17000, 15000]]]
 
-    const [chartFilter, setChartFilter] = useState(1);
-
+    const [chartFilter, setChartFilter] = useState(0);
+    const [isShowingChartPopup, setIsShowingChartPopup] = useState(false);
 
 
     /* CONFIGURE CHART */
     const [chartData, setChartData] = useState({
-        labels: setLabels[chartFilter - 1],
+        labels: setLabels[chartFilter],
         datasets: [
             {
                 label: "Purchases",
-                data: setData[0][chartFilter - 1],
+                data: setData[0][chartFilter],
                 backgroundColor: [
                     "rgba(252, 76, 135, 0.3)",
                 ],
@@ -38,7 +38,7 @@ function Dashboard() {
             },
             {
                 label: "Sales",
-                data: setData[1][chartFilter - 1],
+                data: setData[1][chartFilter],
                 backgroundColor: [
                     "rgba(59, 116, 247,0.3)",
                 ],
@@ -51,34 +51,33 @@ function Dashboard() {
 
     const handleChangeChart = (value) => {
 
-        console.log(value)
-        if (value > 0) {
-            setChartFilter(value)
+        setChartFilter(value)
 
-            setChartData({
-                labels: setLabels[value - 1],
-                datasets: [
-                    {
-                        label: "Purchases",
-                        data: setData[0][value - 1],
-                        backgroundColor: [
-                            "rgba(252, 76, 135, 0.3)",
-                        ],
-                        borderColor: 'rgba(252, 76, 135, 1)',
-                        borderWidth: 3
-                    },
-                    {
-                        label: "Sales",
-                        data: setData[1][value - 1],
-                        backgroundColor: [
-                            "rgba(59, 116, 247,0.3)",
-                        ],
-                        borderColor: "rgba(59, 116, 247, 1)",
-                        borderWidth: 3
-                    }
-                ]
-            })
-        }
+        setChartData({
+            labels: setLabels[value],
+            datasets: [
+                {
+                    label: "Purchases",
+                    data: setData[0][value],
+                    backgroundColor: [
+                        "rgba(252, 76, 135, 0.3)",
+                    ],
+                    borderColor: 'rgba(252, 76, 135, 1)',
+                    borderWidth: 3
+                },
+                {
+                    label: "Sales",
+                    data: setData[1][value],
+                    backgroundColor: [
+                        "rgba(59, 116, 247,0.3)",
+                    ],
+                    borderColor: "rgba(59, 116, 247, 1)",
+                    borderWidth: 3
+                }
+            ]
+        })
+
+        setIsShowingChartPopup(false)
     }
 
     /* CREATE ARROWS SCROLLBAR MOVE */
@@ -86,93 +85,7 @@ function Dashboard() {
 
     const scrollContainerRef2 = useRef(null);
 
-    useEffect(() => {
-        var x, i, j, l, ll, selElmnt, a, b, c;
-        /*look for any elements with the class "custom-select":*/
-        x = document.getElementsByClassName("chart__customselect");
-        l = x.length;
-        for (i = 0; i < l; i++) {
-            if (x[i].classList.contains("initialized")) continue; // Skip already initialized selects
-
-            x[i].classList.add("initialized"); // Mark as initialized
-
-            selElmnt = x[i].getElementsByTagName("select")[0];
-            ll = selElmnt.length;
-            /*for each element, create a new DIV that will act as the selected item:*/
-            a = document.createElement("DIV");
-            a.setAttribute("class", "chart__select-selected");
-            a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-            x[i].appendChild(a);
-            /*for each element, create a new DIV that will contain the option list:*/
-            b = document.createElement("DIV");
-            b.setAttribute("class", "chart__select-items chart__select-hide");
-            for (j = 1; j < ll; j++) {
-                /*for each option in the original select element,
-                create a new DIV that will act as an option item:*/
-                c = document.createElement("DIV");
-                c.innerHTML = selElmnt.options[j].innerHTML;
-                c.addEventListener("click", function (e) {
-                    /*when an item is clicked, update the original select box,
-                    and the selected item:*/
-                    var y, i, k, s, h, sl, yl;
-                    s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-                    sl = s.length;
-                    h = this.parentNode.previousSibling;
-                    for (i = 0; i < sl; i++) {
-                        if (s.options[i].innerHTML == this.innerHTML) {
-                            /* Change Chart */
-                            handleChangeChart(i)
-
-                            s.selectedIndex = i;
-                            h.innerHTML = this.innerHTML;
-                            y = this.parentNode.getElementsByClassName("chart__same-as-selected");
-                            yl = y.length;
-                            for (k = 0; k < yl; k++) {
-                                y[k].removeAttribute("class");
-                            }
-                            this.setAttribute("class", "chart__same-as-selected");
-                            break;
-                        }
-                    }
-                    h.click();
-                });
-                b.appendChild(c);
-            }
-            x[i].appendChild(b);
-            a.addEventListener("click", function (e) {
-                /*when the select box is clicked, close any other select boxes,
-                and open/close the current select box:*/
-                e.stopPropagation();
-                closeAllSelect(this);
-                this.nextSibling.classList.toggle("chart__select-hide");
-                this.classList.toggle("chart__select-arrow-active");
-            });
-        }
-        function closeAllSelect(elmnt) {
-            /*a function that will close all select boxes in the document,
-            except the current select box:*/
-            var x, y, i, xl, yl, arrNo = [];
-            x = document.getElementsByClassName("chart__select-items");
-            y = document.getElementsByClassName("chart__select-selected");
-            xl = x.length;
-            yl = y.length;
-            for (i = 0; i < yl; i++) {
-                if (elmnt == y[i]) {
-                    arrNo.push(i)
-                } else {
-                    y[i].classList.remove("chart__select-arrow-active");
-                }
-            }
-            for (i = 0; i < xl; i++) {
-                if (arrNo.indexOf(i)) {
-                    x[i].classList.add("chart__select-hide");
-                }
-            }
-        }
-        /*if the user clicks anywhere outside the select box,
-        then close all select boxes:*/
-        document.addEventListener("click", closeAllSelect);
-    }, [])
+    const chartPopup = useRef(null);
 
     const handleMoveCard1 = (option) => {
         switch (option) {
@@ -203,6 +116,22 @@ function Dashboard() {
                 break;
         }
     }
+
+    useEffect(() => {
+        /* CLOSE WINDOW WHEN CLICK OUTSIDE */
+        const handleClickOutside = (event) => {
+            if (chartPopup.current && !chartPopup.current.contains(event.target)) {
+                setIsShowingChartPopup(false)
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+
+    }, []);
 
     return (
         <>
@@ -295,14 +224,19 @@ function Dashboard() {
                 <div className='chart-container'>
                     <div className="chart-container__title">
                         <h2>Sales and Purchase Statistics</h2>
-                        <div className="chart__customselect">
-                            <select className="chart__time">
-                                <option value={chartFilter} selected={true}>{chartFilter == 1 ? "Last 30 Days" : chartFilter == 2 ? "Last 90 Days" : chartFilter == 3 ? "Last 6 Months" : chartFilter == 4 ? "Last Year" : "Select a Filter"}</option>
-                                <option value={1}>Last 30 Days</option>
-                                <option value={2}>Last 90 Days</option>
-                                <option value={3}>Last 6 Months</option>
-                                <option value={4}>Last Year</option>
-                            </select>
+                        <div className="chart__customselect" ref={chartPopup}>
+                            <div className="chart-customselect__title" onClick={() => { setIsShowingChartPopup(!isShowingChartPopup) }}>
+                                <p > {chartFilter == 0 ? "Last 30 Days" : chartFilter == 1 ? "Last 90 Days" : chartFilter == 2 ? "Last 6 Months" : chartFilter == 3 ? "Last Year" : "Select a Filter"}</p>
+                                <IoIosArrowDown style={{ display: isShowingChartPopup == true ? "inline-block" : "none" }} />
+                                <IoIosArrowUp style={{ display: isShowingChartPopup == true ? "none" : "inline-block" }} />
+
+                            </div>
+                            <div className="chart__time" style={{ display: isShowingChartPopup == false ? "none" : "flex" }}>
+                                <div className='chart-time__options' onClick={() => handleChangeChart(0)}>Last 30 Days</div>
+                                <div className='chart-time__options' onClick={() => handleChangeChart(1)}>Last 90 Days</div>
+                                <div className='chart-time__options' onClick={() => handleChangeChart(2)}>Last 6 Months</div>
+                                <div className='chart-time__options' onClick={() => handleChangeChart(3)}>Last Year</div>
+                            </div>
                         </div>
                     </div>
                     <LineChartCard chartData={chartData} />
